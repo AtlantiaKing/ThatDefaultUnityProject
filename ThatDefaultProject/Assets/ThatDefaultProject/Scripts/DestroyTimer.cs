922c2da1b9c2840868190c91b78696236041a376
Wardergrip <42802496+Wardergrip@users.noreply.github.com>
2023-12-12 16:45:03 +0100

    Added more functionality to DestroyTimer
    Allowed DestroyTimer to be used through code alone.

--- ThatDefaultProject/Assets/ThatDefaultProject/Scripts/DestroyTimer.cs
+++ ThatDefaultProject/Assets/ThatDefaultProject/Scripts/DestroyTimer.cs
@@ -2,22 +2,22 @@ using UnityEngine;
 
 namespace that
 {
-    public class DestroyTimer : MonoBehaviour
-    {
-        [Tooltip(
-            "Time can only have meaningful change when it is set before the Start of DestroyTimer is called. " +
-            "Start is called the first frame that the script is active. " +
-            "If you want to set the time later, disable the script."
-            )]
-        [SerializeField] private float _time = 1f;
+	public class DestroyTimer : MonoBehaviour
+	{
+		[SerializeField] private float _time = 1f;
+		[SerializeField] private bool _runOnStart = false;
+		public bool RunOnStart { get => _runOnStart; }
 
-        /// <summary>
-        /// Time can only have meaningful change when it is set before the Start of DestroyTimer is called.
-        /// <para>Start is called the first frame that the script is active.</para>
-        /// <para>If you want to set the time later, disable the script.</para>
-        /// </summary>
-        public float Time { get { return _time; } set {  _time = value; } }
+		public float Time { get { return _time; } set { _time = value; } }
 
-        void Start() => Destroy(gameObject, _time);
-    }
+		void Start()
+		{
+			if (RunOnStart) StartTimer();
+		}
+
+		public void StartTimer()
+		{
+			Destroy(gameObject, _time);
+		}
+	}
 }
