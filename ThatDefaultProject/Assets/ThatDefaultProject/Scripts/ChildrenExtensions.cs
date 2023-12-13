ccf75e0490e3ea15d659e5be417b49a3782af792
Wardergrip <42802496+Wardergrip@users.noreply.github.com>
2023-12-13 17:10:09 +0100

    Added children extensions

---
+++ ThatDefaultProject/Assets/ThatDefaultProject/Scripts/ChildrenExtensions.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace that
+{
+    public static class ChildrenExtensions
+    {
+		public static void ForEachChild(this Transform transform, Action<Transform, int> action)
+		{
+			for (int i = 0; i < transform.childCount; ++i)
+			{
+				action(transform.GetChild(i), i);
+			}
+		}
+		public static void ForEachChild(this MonoBehaviour mb, Action<Transform, int> action)
+		{
+			ForEachChild(mb.transform, action);
+		}
+		public static void ForEachChild(this GameObject go, Action<Transform, int> action)
+		{
+			ForEachChild(go.transform, action);
+		}
+		public static void ForEachChild(this Transform transform, Action<Transform> action)
+		{
+			foreach (Transform child in transform)
+			{
+				action(child);
+			}
+		}
+		public static void ForEachChild(this MonoBehaviour mb, Action<Transform> action)
+		{
+			ForEachChild(mb.transform, action);
+		}
+		public static void ForEachChild(this GameObject go, Action<Transform> action)
+		{
+			ForEachChild(go.transform, action);
+		}
+		public static List<Transform> AllChildren(this Transform transform)
+		{
+			List<Transform> list = new();
+			foreach (Transform t in transform)
+			{
+				list.Add(t);
+			}
+			return list;
+		}
+	}
+}
